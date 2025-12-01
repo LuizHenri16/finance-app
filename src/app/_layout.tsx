39a943c05d0ab1b1311eb5b16d9e530d297c1b36
LuizHerni16 <lbastos722@gmail.com>
2025-12-01 17:26:01 -0300

    feature: add FinanceContext

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
+import { FinanceProvider } from "../contexts/FinanceContext";
 
 export default function Layout() {
     return (
-        <Stack initialRouteName="home/index" screenOptions={{headerShown: false}}>
-            <Stack.Screen name="home/index"/>
-            <Stack.Screen name="receita/index"/>
-        </Stack> 
+        <FinanceProvider>
+            <Stack initialRouteName="home/index" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="home/index" />
+                <Stack.Screen name="receita/index" />
+            </Stack>
+        </FinanceProvider>
     )
 }
---
+++ src/contexts/FinanceContext.tsx
@@ -0,0 +1,59 @@
+import { createContext, ReactNode, useState } from "react";
+import { DespesaPersist, DespesaPersisted } from "../types/despesa";
+import { ReceitaPersist, ReceitaPersisted } from "../types/receita";
+
+
+type FinanceContextType = {
+  despesas: DespesaPersisted[];
+  receitas: ReceitaPersisted[];
+  addDespesa: (despesa: DespesaPersist) => void;
+  removeDespesa: (id: number) => void;
+  addReceita: (receita: ReceitaPersist) => void;
+  removeReceita: (id: number) => void;
+  saldo: number;
+};
+
+export const FinanceContext = createContext<FinanceContextType | null>(null);
+
+export function FinanceProvider({ children }: { children: ReactNode }) {
+
+    const [despesas, setDespesas] = useState<DespesaPersisted[]>([]);
+  const [receitas, setReceitas] = useState<ReceitaPersisted[]>([]);
+
+  function addDespesa(despesa: DespesaPersist) {
+    const novaDespesa: DespesaPersisted = { id: Date.now(), ...despesa };
+    setDespesas((prev) => [...prev, novaDespesa]);
+  }
+
+  function removeDespesa(id: number) {
+    setDespesas((prev) => prev.filter((d) => d.id !== id));
+  }
+
+  function addReceita(receita: ReceitaPersist) {
+    const novaReceita: ReceitaPersisted = { id: Date.now(), ...receita };
+    setReceitas((prev) => [...prev, novaReceita]);
+  }
+
+  function removeReceita(id: number) {
+    setReceitas((prev) => prev.filter((r) => r.id !== id));
+  }
+
+  const saldo =
+    receitas.reduce((acc, r) => acc + (r.valor ?? 0), 0) -
+    despesas.reduce((acc, d) => acc + (d.valor ?? 0), 0);
+
+
+    return (
+        <FinanceContext.Provider value={{
+        despesas,
+        receitas,
+        addDespesa,
+        removeDespesa,
+        addReceita,
+        removeReceita,
+        saldo,
+      }}> 
+            {children}
+        </FinanceContext.Provider>
+    );
+}
